c8b48b81453b641a3ecd64cb381ee384de4555c9
jeonyunhoo <jeonyunhu0130@gmail.com>
2026-03-30 14:47:10 +0900

    문자 배열 실습

---
+++ array/char-array-1.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main (void)
+{
+    char str[30] = "C language"; //문자 배열 30자 변수 선언
+    int i = 0;
+
+    while (str[i] != '\0') { //만족할 때 까지 반복 !=, 다르다. \0은 null값 의미 
+                             //문자의 내용이 끝나는 시점에 반드시 null값이 들어감
+
+        i++;
+    }
+
+    printf ("문자열\"%s\"의 길이는 %d입니다.\n", str, i);
+    return 0;
+}
---
+++ array/char-array-2.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main () {
+
+	char fruits[3][20];
+	
+	for (int i = 0; i <= 2; i++) {
+
+		printf ("과일의 이름을 입력하세요: ");
+		scanf ("%s", &fruits[i]);
+		//printf ("\n%d. 당신이 선택한 과일: %s\n", i+1, fruits[i]);
+	}
+
+	printf ("입력된 과일: %s, %s, %s", fruits[0], fruits[1], fruits[2]);
+
+	return 0;
+}
