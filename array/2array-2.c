8a873be11e26db001c0ebe9d5193a98512d9fb96
jeonyunhoo <jeonyunhu0130@gmail.com>
2026-03-30 14:44:35 +0900

    2차원 배열

---
+++ array/2array-2.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	int a;
+	char menu[5][10] = { //5행 10열 사이즈의 배열 선언 (0행 ~ 4행, 0열 ~ 9열)
+
+		"init", //한 행씩 초기값 설정
+		"open",
+		"close",
+		"read",
+		"write"
+	};
+
+	for (a = 0; a < 5; a++) {
+
+		printf ("%d 번째 메뉴: %s \n", a, menu[a]);
+	}
+
+	return 0;
+}
