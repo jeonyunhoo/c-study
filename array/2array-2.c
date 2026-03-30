b76bcac02c66e0327fdf9b997a07e7e54a531b03
jeonyunhoo <jeonyunhu0130@gmail.com>
2026-03-30 15:21:49 +0900

    2차 수정

--- array/2array-2.c
+++ array/2array-2.c
@@ -4,16 +4,16 @@
 int main() {
 
 	int a;
-	char menu[5][10] = { //5행 10열 사이즈의 배열 선언 (0행 ~ 4행, 0열 ~ 9열)
+	char menu[3][10] = { //5행 10열 사이즈의 배열 선언 (0행 ~ 4행, 0열 ~ 9열)
 
 		"init", //한 행씩 초기값 설정
 		"open",
 		"close",
-		"read",
-		"write"
+		/*"read",
+		"write"*/
 	};
 
-	for (a = 0; a < 5; a++) {
+	for (a = 0; a < 3; a++) {
 
 		printf ("%d 번째 메뉴: %s \n", a, menu[a]);
 	}
